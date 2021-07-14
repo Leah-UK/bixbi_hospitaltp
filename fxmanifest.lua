fe51cb743dd1b9af47b29491e22f059a0fd6af70
Leah <mail@leahuk.com>
2021-07-14 18:19:43 +0100

    Version Check URL

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,8 @@ Creation Date:	15/03/21
 fx_version 'adamant'
 game 'gta5'
 author 'Leah#0001'
-version '1.1'
+version '1.0'
+versioncheck 'https://raw.githubusercontent.com/Leah-UK/bixbi_hospitaltp/main/fxmanifest.lua'
 
 client_scripts {
     'config.lua', 
